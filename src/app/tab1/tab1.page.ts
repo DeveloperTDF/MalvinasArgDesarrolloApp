f3ac0a8b270b40a71c581603340984550f9ec3b4
DeveloperTDF <appdeveloper230379@gmail.com>
2026-08-25 17:53:10 -0300

    final

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,89 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonButton,
+  IonIcon
+} from '@ionic/angular';
+
+import { addIcons } from 'ionicons';
+import {
+  heartCircleOutline,
+  personOutline,
+  waterOutline,
+  informationCircleOutline,
+  callOutline,
+  womanOutline,
+  manOutline,
+  peopleOutline,
+  schoolOutline,
+  call
+} from 'ionicons/icons';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardContent,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonButton,
+    IonIcon
+  ]
+})
+export class Tab1Page {
+  constructor() {
+    addIcons({
+      heartCircleOutline,
+      personOutline,
+      waterOutline,
+      informationCircleOutline,
+      callOutline,
+      womanOutline,
+      manOutline,
+      peopleOutline,
+      schoolOutline,
+      call
+    });
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 // import { Component } from '@angular/core';
 // import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular';
 // import { ExploreContainerComponent } from '../explore-container/explore-container.component';
@@ -11,16 +97,17 @@
 // export class Tab1Page {
 //   constructor() {}
 // }
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+// import { CommonModule } from '@angular/common';
 
-@Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
-  imports: [CommonModule],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
-})
-export class Tab1Page {
-  constructor() {}
-}
+// @Component({
+//   selector: 'app-tab1',
+//   templateUrl: 'tab1.page.html',
+//   styleUrls: ['tab1.page.scss'],
+//   imports: [CommonModule],
+//   schemas: [CUSTOM_ELEMENTS_SCHEMA]
+// })
+// export class Tab1Page {
+//   constructor() {}
+// }
+
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,17 +1,65 @@
 // import { Component } from '@angular/core';
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+// import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+// import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular';
+// import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+
+// @Component({
+//   selector: 'app-tab2',
+//   templateUrl: 'tab2.page.html',
+//   styleUrls: ['tab2.page.scss'],
+//   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+//   schemas: [CUSTOM_ELEMENTS_SCHEMA]
+// })
+// export class Tab2Page {
+
+//   constructor() {}
+
+// }
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonItem,
+  IonLabel,
+  IonIcon
+} from '@ionic/angular';
+
+import { addIcons } from 'ionicons';
+import { qrCodeOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardContent,
+    IonItem,
+    IonLabel,
+    IonIcon
+  ]
 })
 export class Tab2Page {
-
-  constructor() {}
-
+  constructor() {
+    addIcons({ qrCodeOutline });
+  }
 }
